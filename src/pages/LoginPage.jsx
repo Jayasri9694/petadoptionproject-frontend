f69b706e28ab2f84f7ba9181881c0c61c7dd06ed
Jayashree.k <jaisrijaya75@gmail.com>
2024-12-05 19:51:56 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,10 +4,17 @@ import LoginForm from '../components/LoginForm';
 const LoginPage = () => {
   return (
     <div>
-      <h2>Login</h2>
       <LoginForm />
+      <p> Dont have an account? <a href="/signup">Sign up</a>
+        </p>
+      <p>Forgot your password? <a href="/reset-password">Reset Password</a>
+      </p>
+    <p> <a href="/">Back to Home</a>
+    </p>
+  
+      
     </div>
   );
-};
+}
 
 export default LoginPage;
