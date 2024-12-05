9a3ff7c82801517c7afe5911556f445c7dea5a5b
Jayashree.k <jaisrijaya75@gmail.com>
2024-12-05 19:51:18 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const HomePage = () => {
   return (
     <div className="container-fluid">
-      <h1>Welcome to the Pet Adoption Platform</h1>
-      <img src="https://wallpaperaccess.com/full/5595887.jpg" className="img-fluid" alt="..."></img>
+        <h1>Welcome to the Pet Adoption Platform</h1>
+        <p>Find your perfect furry companion today!</p>
+        <img src="https://wallpaperaccess.com/full/5595887.jpg" className="img-fluid" alt="..."></img>
     </div>
   );
-};
 
+};
 export default HomePage;
