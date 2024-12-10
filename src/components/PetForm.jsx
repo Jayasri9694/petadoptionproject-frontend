35887844e915d24270f72291742266b2b1de9937
jaya <kjayashree061@gmail.com>
2024-12-10 12:16:53 +0530

    commit

--- src/components/PetForm.jsx
+++ src/components/PetForm.jsx
@@ -21,7 +21,7 @@ const PetForm = () => {
     if (image) formData.append("image", image);
 
     try {
-      const response = await axios.post("/api/pets", formData, {
+      const response = await axios.post("http://localhost:5000/api/pets", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
