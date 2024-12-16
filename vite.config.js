702a5fc7f6de2c798267c295bd5d54a96a16a68d
jaya <kjayashree061@gmail.com>
2024-12-16 10:18:59 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173, // Replace 3000 with your desired port number
-    base: '/', // Set the base URL to an empty string to serve the app from the root directory
   },
 })
