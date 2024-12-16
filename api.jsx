4261b335b60fea3764eb5d3357fba9914133f7cd
jaya <kjayashree061@gmail.com>
2024-12-16 12:33:32 +0530

    commit

--- api.jsx
+++ api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-petadoption-4.onrender.com/api',
+  baseURL: 'https://backend-petadoption-4.onrender.com',
 });
 
 export default api;
